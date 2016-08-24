31565d5116950cef235e8d02c5e0c10017e831c7
rthier <u9vata@gmail.com>
2016-08-24 10:51:33 +0200

    Fixed wincompat.h so that it builds with non-msvc...

--- wincompat.h
+++ wincompat.h
@@ -1,4 +1,7 @@
 #pragma once
+#ifndef OBJMASTER_WIN_COMPAT
+#define OBJMASTER_WIN_COMPAT
+
 // This file is intended to add various clever or dirty tricks to achieve windows MSVC compatibility...
 // Only do these things if the compiler is M$C...
 #ifdef _MSC_VER
@@ -6,4 +9,6 @@
 #define strdup _strdup
 // strtop_r is not present in MSVC, but there is this random method that basically do the same...
 #define strtok_r strtok_s
+#endif
+
 #endif
