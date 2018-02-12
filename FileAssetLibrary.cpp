7e20b65f5979680d54cc077acf26e82b6fe16a78
Thier Richárd <rthier@grepton.hu>
2018-02-12 16:03:32 +0100

    Added some compatibility for unicode in the FileAssetLibrary - for my testing it seems to support only 16-bit BMP (Basic Multilingual Plane) of the unicode, but testing is hard and maybe input was wrong in the tests. Code supports it so it might depend on windows version too...

--- FileAssetLibrary.cpp
+++ FileAssetLibrary.cpp
@@ -1,16 +1,45 @@
+#include "wincompat.h"
 #include "FileAssetLibrary.h"
+#include "objmasterlog.h"
 #include <istream>
 #include <fstream>
 #include <string>
 #include <memory>
+#include <cerrno> /* for strerror(errno) */
+
+// The maximum size of error messages to print on error logging
+#define ERR_MSG_SIZE 512
 
 namespace ObjMaster {
+
 	std::unique_ptr<std::istream> FileAssetLibrary::getAssetStream(const char *path, const char *assetFileName) const {
 		// We are just concatenating the two values
-		const std::string fullPath = std::string(path) + assetFileName;
-
+		const std::string spath(path);
+		const std::string sassetFileName(assetFileName);
+		const std::string utf8FullPath  = spath + sassetFileName;
+		OMLOGI("Opening file at %s ...", utf8FullPath.c_str());
+		OMLOGI(" - path %s ...", spath.c_str());
+		OMLOGI(" - assetFileName %s ...", sassetFileName.c_str());
 		// And returning the ifstream to it
-		std::unique_ptr<std::istream> assetStream = std::make_unique<std::ifstream>(std::ifstream(fullPath));
+		std::unique_ptr<std::istream> assetStream = std::make_unique<std::ifstream>(std::ifstream(
+#ifdef _MSC_VER
+			// Rem.: this can fail if the character cannot be represented with 16bit unicodes and this got translated to winapi that only support 16bit UNICODE spaces....
+			// Rem.: When cross compiling with MSVC to android and stuff this works at least for ASCII - I could not test what happens in that case :-(
+			UtfHelper::utf8_to_utf16(utf8FullPath) // MSVC has override for wstring paths - this is not standard however!
+#else
+			utf8FullPath				// Every normal operating system handles UTF-8 as-is, so in other cases we just do this
+#endif
+		));
+
+		if (assetStream->fail()) {
+			char errMsgHolder[ERR_MSG_SIZE];
+			// We only log about failure. The stream we return will act as empty so we will end up parsing an empty obj
+			// If this is not what you wish for, you can implement your own similar asset library on your own...
+			strerror_s(errMsgHolder, ERR_MSG_SIZE, errno);
+			OMLOGE("...Cannot open file because: %s", errMsgHolder);
+		} else {
+			OMLOGI("...Successfully opened %s%s", path, assetFileName);
+		}
 		return assetStream;
 	}
 } 
--- wincompat.h
+++ wincompat.h
@@ -1,10 +1,13 @@
-#pragma once
 #ifndef OBJMASTER_WIN_COMPAT
 #define OBJMASTER_WIN_COMPAT
 
 // This file is intended to add various clever or dirty tricks to achieve windows MSVC compatibility...
 // Only do these things if the compiler is M$C...
 #ifdef _MSC_VER
+
+#include <vector>
+#include <string>
+#include <stdexcept>
 // Silly hax for getting rid of errors arising from missing proper posix method names on windows MSVC...
 #define strdup _strdup
 // strtop_r is not present in MSVC, but there is this random method that basically do the same...
@@ -16,6 +19,92 @@ static char *windows_hacky_strncpy(char *dest, const char *src, size_t n) {
 	strncpy_s(dest, n * sizeof(char), src, n);
 	return dest;
 }
+
+/** Various helper functions for UTF-8 and UTF-16 (mostly only needed on windows) */
+class UtfHelper {
+public:
+	/** Converts an utf8 string to utf16 - one could use codecvt but many platforms deprecate it completely or plain miss implementation for that */
+	// Rem.: Please do not use wstring at all when on "proper" operating systems - like *nix, *bsd, android, emscripten/webgl etc...
+	static inline std::wstring utf8_to_utf16(const std::string& utf8)
+	{
+		std::vector<unsigned long> unicode;
+		size_t i = 0;
+		while (i < utf8.size())
+		{
+			unsigned long uni;
+			size_t todo;
+			bool error = false;
+			unsigned char ch = utf8[i++];
+			if (ch <= 0x7F)
+			{
+				uni = ch;
+				todo = 0;
+			}
+			else if (ch <= 0xBF)
+			{
+				throw std::logic_error("not a UTF-8 string");
+			}
+			else if (ch <= 0xDF)
+			{
+				uni = ch & 0x1F;
+				todo = 1;
+			}
+			else if (ch <= 0xEF)
+			{
+				uni = ch & 0x0F;
+				todo = 2;
+			}
+			else if (ch <= 0xF7)
+			{
+				uni = ch & 0x07;
+				todo = 3;
+			}
+			else
+			{
+				throw std::logic_error("not a UTF-8 string");
+			}
+			for (size_t j = 0; j < todo; ++j)
+			{
+				if (i == utf8.size())
+					throw std::logic_error("not a UTF-8 string");
+				unsigned char ch = utf8[i++];
+				if (ch < 0x80 || ch > 0xBF)
+					throw std::logic_error("not a UTF-8 string");
+				uni <<= 6;
+				uni += ch & 0x3F;
+			}
+			if (uni >= 0xD800 && uni <= 0xDFFF) {
+				// FIXME: Windows enables path names with these although they are not standard. What to do? I choose not to implement them...
+				throw std::logic_error("not a UTF-8 string");
+			}
+			if (uni > 0x10FFFF)
+				throw std::logic_error("not a UTF-8 string");
+			unicode.push_back(uni);
+		}
+		std::wstring utf16;
+		for (size_t i = 0; i < unicode.size(); ++i)
+		{
+			unsigned long uni = unicode[i];
+			if (uni <= 0xFFFF)
+			{
+				utf16 += (wchar_t)uni;
+			}
+			else
+			{
+				// Support for the whole code-space is here (bigger than 16 bit values)
+				uni -= 0x10000;
+				// Unicode is a 20 bits wide code-space
+				// When we need more than 16 bits
+				// we first take the upper 10 bits and add that to 0xD800 => first char
+				utf16 += (wchar_t)((uni >> 10) + 0xD800);
+				// Then we take the lower 10 bits by and-ing with 0x3FF and add 0xDC00 => second char
+				utf16 += (wchar_t)((uni & 0x3FF) + 0xDC00);
+				// See: https://en.wikipedia.org/wiki/UTF-16#U+D800_to_U+DFFF
+			}
+		}
+		return utf16;
+	}
+};
 #endif
 
 #endif
