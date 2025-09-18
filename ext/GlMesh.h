7845ee81c8642783b37002f47600899a9000a167
Richard Thier <magosit@outlook.hu>
2025-09-18 12:59:06 +0200

    fixed the refactor for VAO, can use GL3.3 core where VAO is mandatory and FULL_GL mode enables it, fixed non-vao mode because AI was dumb as shit and hallucinated that I do not need to re-send attribute layout (but I do  - see original code)

--- ext/GlMesh.h
+++ ext/GlMesh.h
@@ -2,7 +2,13 @@
 #define OM_GLMESH_H
 
 /* An opengl mesh prepared to render and updated to the GPU. */
-/* Set -DUSE_OES_VAO to have a "vao" */
+/* Set -DUSE_VAO to have a "vao" even in ES and webgl 1.x, but USE_FULL_GL enables it if there is no PRE_33_GL (compatibility) */
+
+#ifdef USE_FULL_GL
+#ifndef PRE_33_GL
+#define USE_VAO // VAO is mandatory in 3.3+ opengl core contexts but not in compatibility!
+#endif /* PRE_33_GL */
+#endif /* USE_FULL_GL */
 
 #define GL_GLEXT_PROTOTYPES
 /* Needs something like:*/
@@ -21,16 +27,56 @@ namespace ObjMaster {
 
 	/** A mesh that is uploaded to the GPU - TODO: need to add destructor to remove from GPU? */
 	class GlMesh {
+		bool uploaded = false;
+		GLsizei indexCount = 0;
 		TextureDataHoldingMaterial *material;
+		GLuint position_loc;
+		GLuint normal_loc;
+		GLuint texCoord_loc;
 		GLuint vbo = 0;
 		GLuint ibo = 0;
-		GLsizei indexCount = 0;
-		bool uploaded = false;
-#ifdef USE_OES_VAO
+#ifdef USE_VAO
 		GLuint vao = 0; // if using VAOs via OES_vertex_array_object
-#endif
+#else
+		size_t posOffset = 0;
+		size_t normalOffset = 0;
+		size_t texCoordOffset = 0;
+#endif /* USE_VAO */
 
 	public:
+		/**
+		 * Sets vertex attribute pointers for this mesh data and enables them
+		 */
+		static inline void setup_vertex_attributes(
+				GLuint position_loc,
+				GLuint normal_loc,
+				GLuint texCoord_loc,
+				size_t posOffset,
+				size_t normalOffset,
+				size_t texCoordOffset) {
+
+			// By design, we know that the positions are the first elements in the VertexStructure
+			// so we can use zero as the pointer/index in the vertex data!
+			glVertexAttribPointer(position_loc, 3, GL_FLOAT, GL_FALSE, sizeof(VertexStructure), (const GLvoid *) posOffset);
+			printGlError("after position attrib pointer");
+
+			// Use the calculated offset for getting the pointer to the normals in the vertex data
+			glVertexAttribPointer(normal_loc,
+					3, GL_FLOAT, GL_FALSE, sizeof(VertexStructure), (const GLvoid *)(normalOffset * 4));
+			printGlError("after normal attrib pointer");
+
+			// Use the calculated offset for getting the pointer to the texcoords in the vertex data
+			glVertexAttribPointer(texCoord_loc,
+					2, GL_FLOAT, GL_FALSE, sizeof(VertexStructure), (const GLvoid *)(texCoordOffset * 4));
+			printGlError("after texcoord attrib pointer");
+
+			// Enable the vertex attributes as arrays
+			glEnableVertexAttribArray(position_loc);
+			glEnableVertexAttribArray(normal_loc);
+			glEnableVertexAttribArray(texCoord_loc);
+			printGlError("after enabling vertex attributes");
+		}
+
 		/** Empty mesh */
 		inline GlMesh() noexcept {}
 
@@ -48,7 +94,8 @@ namespace ObjMaster {
 				GLuint normalLoc,
 				GLuint texCoordLoc,
 				TextureDataHoldingMaterial *mat,
-				ObjMaster::ObjMeshObject &mesh) noexcept : material(mat) {
+				ObjMaster::ObjMeshObject &mesh) noexcept
+				: material(mat), position_loc(positionLoc), normal_loc(normalLoc), texCoord_loc(texCoordLoc) {
 
 			printGlError("Before setup_buffers");
 			if(mesh.inited && (mesh.vertexCount > 0) && (mesh.indexCount > 0)) {
@@ -66,41 +113,59 @@ namespace ObjMaster {
 						&((*(mesh.indices))[0]),
 						GL_STATIC_DRAW);
 
-#ifdef USE_OES_VAO
+#ifdef USE_VAO
+				// Use VAO if present
+				glGenVertexArrays(1, &vao);
+				glBindVertexArray(vao);
+				// Binding the buffer should be part of VAO state
+				glBindBuffer(GL_ARRAY_BUFFER, vbo);
+#ifdef USE_FULL_GL
+#else
 				// Use VAO if present (WebGL: OES_vertex_array_object)
 				glGenVertexArraysOES(1, &vao);
 				glBindVertexArrayOES(vao);
-#endif
-
-				// Bind the vertex buffer object and create two vertex attributes from the bound buffer
+				// Binding the buffer should be part of VAO state
 				glBindBuffer(GL_ARRAY_BUFFER, vbo);
-				// By design, we know that the positions are the first elements in the VertexStructure
-				// so we can use zero as the pointer/index in the vertex data!
-				glVertexAttribPointer(positionLoc, 3, GL_FLOAT, GL_FALSE, sizeof(VertexStructure), 0);
+#endif /* USE_FULL_GL */
+				auto posOffset = 0;
 				// Calculate the offset where the normal vector data starts in the vertex data
 				// This is much better than writing "3" as this handles changes in the structure...
 				auto normalOffset = (&((*(mesh.vertexData))[0].i) - &((*(mesh.vertexData))[0].x));
-				// Use the calculated offset for getting the pointer to the normals in the vertex data
-				glVertexAttribPointer(normalLoc,
-						3, GL_FLOAT, GL_FALSE, sizeof(VertexStructure), (const GLvoid *)(normalOffset * 4));
 				// Calculate the offset where the normal vector data starts in the vertex data
 				// This is much better than writing "3" as this handles changes in the structure...
 				auto texCoordOffset = (&((*(mesh.vertexData))[0].u) - &((*(mesh.vertexData))[0].x));
-				// Use the calculated offset for getting the pointer to the texcoords in the vertex data
-				glVertexAttribPointer(texCoordLoc,
-						2, GL_FLOAT, GL_FALSE, sizeof(VertexStructure), (const GLvoid *)(texCoordOffset * 4));
+#else
+				posOffset = 0;
+				// Calculate the offset where the normal vector data starts in the vertex data
+				// This is much better than writing "3" as this handles changes in the structure...
+				normalOffset = (&((*(mesh.vertexData))[0].i) - &((*(mesh.vertexData))[0].x));
+				// Calculate the offset where the normal vector data starts in the vertex data
+				// This is much better than writing "3" as this handles changes in the structure...
+				texCoordOffset = (&((*(mesh.vertexData))[0].u) - &((*(mesh.vertexData))[0].x));
+#endif /* USE_VAO */
+				printGlError("after buffer creation and binding");
 
-				// Enable the vertex attributes as arrays
-				glEnableVertexAttribArray(positionLoc);
-				glEnableVertexAttribArray(normalLoc);
-				glEnableVertexAttribArray(texCoordLoc);
+				// This state will go into the VAO when using one, otherwise because
+				// VBO and IBO does NOT store this, need to happen every draw of us.
+				setup_vertex_attributes(
+						position_loc,
+						normal_loc,
+						texCoord_loc,
+						posOffset,
+						normalOffset,
+						texCoordOffset);
 
 				// Bind the index buffer object we have created - this is needed so VAO captures it being bound
 				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+				printGlError("after binding ibo in setup");
 
-#ifdef USE_OES_VAO
+#ifdef USE_VAO
+#ifdef USE_FULL_GL
+				glBindVertexArray(0);
+#else
 				glBindVertexArrayOES(0);
-#endif
+#endif /* USE_FULL_GL */
+#endif /* USE_VAO */
 				// Unbind to avoid accidental changes
 				glBindBuffer(GL_ARRAY_BUFFER, 0);
 				/*if (!vao)*/ glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
@@ -143,47 +208,79 @@ namespace ObjMaster {
 	};
 
 	/** If you create a binding to a GlMesh, it is prepared for drawing! RAII releases bindings */
-	struct GlMeshBinding {
-		bool uploaded = false;
-		TextureDataHoldingMaterial *material;
-		GLuint vbo;
-		GLuint ibo;
-		GLsizei indexCount;
-#ifdef USE_OES_VAO
+	class GlMeshBinding {
+	private:
+		GLuint vbo = 0;
+		GLuint ibo = 0;
+#ifdef USE_VAO
 		GLuint vao = 0; // if using VAOs via OES_vertex_array_object
+#else
+		size_t posOffset = 0;
+		size_t normalOffset = 0;
+		size_t texCoordOffset = 0;
 #endif
+	public:
+		bool uploaded = false;
+		GLsizei indexCount = 0;
+		GLuint position_loc;
+		GLuint normal_loc;
+		GLuint texCoord_loc;
+		TextureDataHoldingMaterial *material;
 
 		/** Binds a GlMesh so you can draw these */
 		inline GlMeshBinding(GlMesh &mesh) noexcept :
-				uploaded(mesh.uploaded),
-				material(mesh.material),
 				vbo(mesh.vbo),
 				ibo(mesh.ibo),
-				indexCount(mesh.indexCount)
-#ifdef USE_OES_VAO
-				,vao(mesh.vao)
-#endif
-				{
-#ifdef USE_OES_VAO
-			glBindVertexArrayOES(vao);
-			glBindVertexArrayOES(0);
+#ifdef USE_VAO
+				vao(mesh.vao),
 #else
-			// Without VAO, still very cheap: only bind VBO/IBO if not already bound
+				posOffset(mesh.posOffset),
+				normalOffset(mesh.normalOffset),
+				texCoordOffset(mesh.texCoordOffset),
+#endif
+				uploaded(mesh.uploaded),
+				indexCount(mesh.indexCount),
+				position_loc(mesh.position_loc),
+				normal_loc(mesh.normal_loc),
+				texCoord_loc(mesh.texCoord_loc),
+				material(mesh.material) {
+#ifdef USE_VAO
+#ifdef USE_FULL_GL
+			glBindVertexArray(vao);
+#else
+			glBindVertexArrayOES(vao);
+#endif /* USE_FULL_GL */
+#else
+			// With no VAO support its more pricy
+			// because: 2 buffer bindings + attribute setup both needed!
 			glBindBuffer(GL_ARRAY_BUFFER, vbo);
 			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-#endif
+
+			// This state will go into the VAO when using one, otherwise because
+			// VBO and IBO does NOT store this, need to happen every draw of us.
+			GlMesh::setup_vertex_attributes(
+					position_loc,
+					normal_loc,
+					texCoord_loc,
+					posOffset,
+					normalOffset,
+					texCoordOffset);
+#endif /* USE_VAO */
 		}
 
 		inline ~GlMeshBinding() noexcept {
-#ifdef USE_OES_VAO
+#ifdef USE_VAO
+#ifdef USE_FULL_GL
+			glBindVertexArray(0);
+#else
 			glBindVertexArrayOES(0);
+#endif /* USE_FULL_GL */
 #else
 			// Without VAO, still very cheap: only unbind VBO/IBO if not already bound
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
 			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-#endif
+#endif /* USE_VAO */
 		}
 	};
-
-	}
+}
 #endif /* OM_GLMESH_H */
